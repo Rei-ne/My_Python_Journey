d5e44cf371fb184dc42e389f385008ad899206db
Toyosi <reinetoyosii@gmail.com>
2022-01-10 00:32:49 +0100

    updates

--- SnakebiteToD.py
+++ SnakebiteToD.py
@@ -1,17 +1,19 @@
 from random import choice
 
-list1 = ['Sit on a couch', 'Do a handstand', 'Jump up and down', 'Demonstrate yoga', 'Act like Darth Vader is choking you',
-         'Do 5 pushups', 'Lay down', 'Put on a hat', 'Drink a cup of water', 'Touch the wall']
-list2 = ['while picking you nose', 'while holding you nose', 'while singing', 'while tapping your head', 'while pregnant',
-         'while singing jinglebells', 'while drinking water', 'while laughing', 'while clapping your hands', 'while stomping your feet']
-list3 = ['your mom', 'your best freind', 'the wall',
-         'a cat', 'a keyboard', 'your neighbor', 'a phone']
+list1 = ['Sit on a couch', 'Jump up and down', 'Demonstrate yoga', 'Act like Buhari',
+         'Do 5 pushups', 'Lay down', 'Take 3 shots', 'Drink a bottle of water', 'Dance to any song']
+list2 = ['while picking your nose', 'while scrolling on your phone', 'while singing', 'while tapping your head', 'while pregnant',
+         'while singing jinglebells', 'while shaking your butt', 'while laughing', 'while clapping your hands', 'while stomping your feet']
+list3 = ['your mom', 'your best friend',
+         'the wall', 'the mirror', 'everyone', ]
 
 
 def my_game():
     continue_to_play = True
     while continue_to_play:
-        count = int(input("How many dares would you like: "))
+        print("Welcome to Reine\'s Python Snakebite Dare game!")
+        print("These are the rules of the game:\nYou can pick from a number of 1-10 dares at a time")
+        count = int(input("How many dares would you like?: "))
         if count < 1 or count > 10:
             print("Maximum of 10 dares and minimum of 1 dare allowed!")
         elif count <= 10:
@@ -24,8 +26,8 @@ def my_game():
         if to_play == 'y':
             my_game()
         else:
-            print("{:s}".format("Thank you for playing"))
             continue_to_play = False
+            print("{:s}".format("Thank you for playing"))
 
 
 my_game()
